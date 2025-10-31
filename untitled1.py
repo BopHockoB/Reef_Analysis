2818162106834503d2a37649da4f75d414ed0b68
BopHockoB <zemskiy1255@gmail.com>
2025-10-31 20:05:54 +0000

    Multicollinearity explored and run 1 Lasso trial. Also task was swapped from classification to regression

--- untitled1.py
+++ untitled1.py
@@ -25,8 +25,28 @@ filename_read_algae = os.path.join(path, "invertebrate_algae_data.csv")
 
 df_fish = pd.read_csv(filename_read_fish)
 
+'''
+A_ord	Allometric constant (intercept in length–weight model)	
+#Converts fish length to mass
+
+B_pen	Allometric exponent (scaling factor in model)	
+Defines growth shape and body proportion
+
+They are fish related columns to describe their size and weight 
+(remove in case enviroment prediction)
+'''
+
+df_fish_prep = df_fish.drop(['label', 'year', 'month', 'day',
+              'region', 'island', 'reef', 'latitude',
+              'longitude', 'habitat', 'phylum',
+              'taxa1', 'taxa2', 'taxa3', 'family',
+              'trophic_group','a_ord', 'b_pen',
+              'id_reef','transect', 'area'], axis=1)
+
 
 df_algae = pd.read_csv(filename_read_algae)
+
+
 
 print (df_algae['label'].unique())
 '''
@@ -36,6 +56,20 @@ print (df_algae['label'].unique())
 We can make data cleaner by removing SUB.
 Healthy Reef: Coral-dominated (high hard coral cover, low macroalgae)
 '''
+
+df_algae = df_algae[df_algae['label'] != "SUB"]
+df_algae_prep = df_algae.drop(['label', 'year', 'month', 'day',
+              'region', 'island', 'reef', 'latitude',
+              'longitude', 'habitat', 'phylum',
+              'taxa1', 'taxa2', 'taxa3', 'transect'],
+                              axis=1)
+
+'''
+Multiple instance learning?
+We need to define what has bigger influence in reef health (which dataframe to enrich)
+'''
+
+
 
 algae = df_algae[df_algae['label'] == 'ALG']
 invertebrate = df_algae[df_algae['label'] == 'INV']
@@ -87,17 +121,22 @@ species_side_pivot = species_side_counts.pivot(
 
 
 
-species_side_pivot.plot(
+ax = species_side_pivot.plot(
     kind="bar", 
     figsize=(16,6), 
-    width=0.8,
+    width=1,
 )
+
 
 plt.title("Fish Species Counts by Reef Side")
 plt.xlabel("Species")
 plt.ylabel("Count")
 plt.xticks(rotation=90)
 plt.legend(title="Reef Side")
+
+for i in range(len(species_side_pivot)):
+    ax.axvline(i - 0.5, color="lightgray", linestyle="-", linewidth=0.7)
+
 plt.tight_layout()
 plt.show()
 
@@ -106,14 +145,36 @@ plt.show()
 
 
 '''
-We can drop next features:
-Year, month, day, lat, long, phylum, taxa1, taxa2, taxa3, species, 
-
+Define corellation between species presence and side protectrion status
 '''
 
+protection_status_side_count = (
+    df_algae[df_algae['label'] != "SUB"]
+    .groupby(["protection_status", "side"])
+    .size()
+    .reset_index(name="count")
+    )
+
+protection_status_fish_species = df_algae.join(other=df_fish, 
+                                               on="reef_joined",
+                                               how="outer",
+                                               lsuffix="_algae",
+                                               rsuffix="_fish")
 
 
+status_fish_correlation = (
+    protection_status_fish_species.groupby(["species_fish", "protection_status_algae"])
+    .size()
+    .reset_index(name="count")
+    )
+
+fish_trophic_map = protection_status_fish_species[["species_fish", "trophic_level_fish"]].drop_duplicates()
 
 
+status_fish_correlation = status_fish_correlation.merge(
+    fish_trophic_map,
+    on="species_fish",
+    how="left"
+)
 
 
